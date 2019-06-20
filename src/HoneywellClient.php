e546dd477e504ac75cccf41b13bc74bfaf3b0fb9
Mike Bouwmans <mike@fruitcake.nl>
2019-06-20 15:53:37 +0200

    Authentication is working

--- src/HoneywellClient.php
+++ src/HoneywellClient.php
@@ -5,6 +5,7 @@ namespace Fruitcake\Honeywell;
 
 use Fruitcake\Honeywell\Models\HoneywellAccessCredentials;
 use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
 
 
 class HoneywellClient
@@ -45,15 +46,25 @@ class HoneywellClient
     protected $consumerSecret;
 
     /**
+     * @var callable $onCredentialsCallback
+     */
+    protected $onCredentialsCallback;
+
+    /**
      * HoneywellClient constructor.
      *
-     * @param Client $httpClient
-     * @param string $redirectUri
-     * @param string $consumerKey
-     * @param string $consumerSecret
+     * @param  Client  $httpClient
+     * @param  string  $redirectUri
+     * @param  string  $consumerKey
+     * @param  string  $consumerSecret
      */
-    public function __construct($redirectUri, $consumerKey, $consumerSecret, Client $httpClient = null, HoneywellAccessCredentials $accessCredentials = null)
-    {
+    public function __construct(
+        $redirectUri,
+        $consumerKey,
+        $consumerSecret,
+        HoneywellAccessCredentials $accessCredentials = null,
+        Client $httpClient = null
+    ) {
         $this->redirectUri = $redirectUri;
         $this->consumerKey = $consumerKey;
         $this->consumerSecret = $consumerSecret;
@@ -77,15 +88,14 @@ class HoneywellClient
         if (!$this->accessCredentials->getAuthorizationCode() && !request('code')) {
             $this->redirectToAuthorizationPage();
         }
+
         if (!$this->accessCredentials->getRefreshToken() && !$this->accessCredentials->getAccessToken()) {
             $this->requestAccessToken();
 
         }
-
         if ($this->accessCredentials->accessTokenIsExpired() && $this->accessCredentials->getRefreshToken()) {
-            $this->requestAccessToken($this->accessCredentials->getRefreshToken());
+            $this->refreshAccessToken();
         }
-
 
         return $this;
     }
@@ -107,19 +117,40 @@ class HoneywellClient
     /**
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    private function requestAccessToken()
+    private function refreshAccessToken()
     {
-
         $response = $this->httpClient->request('POST', $this->tokenUrl, [
             'form_params' => [
-                'code' => $this->accessCredentials->getAuthorizationCode(),
-                'grant_type' => 'authorization_code',
+                'refresh_token' => $this->accessCredentials->getRefreshToken(),
+                'grant_type' => 'refresh_token',
+                'redirect_uri' => $this->redirectUri,
             ],
             'headers' => [
                 'Authorization' => sprintf('Basic %s', $this->getBase64Key()),
             ],
         ]);
-        dd($response);
+
+        $this->toAccessCredentials($response);
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    private function requestAccessToken()
+    {
+        $response = $this->httpClient->request('POST', $this->tokenUrl, [
+            'form_params' => [
+                'code' => $this->accessCredentials->getAuthorizationCode(),
+                'grant_type' => 'authorization_code',
+                'redirect_uri' => $this->redirectUri,
+            ],
+            'headers' => [
+                'Authorization' => sprintf('Basic %s', $this->getBase64Key()),
+            ],
+        ]);
+
+        $this->toAccessCredentials($response);
+
     }
 
     /**
@@ -128,6 +159,31 @@ class HoneywellClient
     private function getBase64Key()
     {
         return base64_encode(sprintf('%s:%s', $this->consumerKey, $this->consumerSecret));
+    }
+
+    /**
+     * @param  callable  $onCredentialsCallback
+     *
+     * @return HoneywellClient
+     */
+    public function setOnCredentialsCallback(callable $onCredentialsCallback) : HoneywellClient
+    {
+        $this->onCredentialsCallback = $onCredentialsCallback;
+
+        return $this;
+    }
+
+    /**
+     * @param  ResponseInterface  $response
+     */
+    private function toAccessCredentials(ResponseInterface $response) : void
+    {
+        $body = json_decode($response->getBody());
+        $this->accessCredentials->setAccessToken($body->access_token);
+        $this->accessCredentials->setRefreshToken($body->refresh_token);
+        $this->accessCredentials->setTokenExpireTime(time() + $body->expires_in);
+
+        ($this->onCredentialsCallback)($this->accessCredentials);
     }
 
 
